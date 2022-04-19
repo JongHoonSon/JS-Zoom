76557b9702649e78c59a1e90e4ab2f946fa81918
JongHoonSon <roka1684@gmail.com>
2022-04-19 17:33:22 +0900

    #1.6 Chat Completed
    웹소켓 서버가 유저로부터 받은 message를 다시 유저에게 돌려주도록 설정

--- src/server.js
+++ src/server.js
@@ -17,7 +17,9 @@ const wss = new WebSocket.Server({ server });
 wss.on("connection", (socket) => {
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (message) => console.log(message.toString("utf8")));
+  socket.on("message", (message) => {
+    socket.send(message);
+  });
   socket.send("Hello! I'm WebSocket");
 });
 
