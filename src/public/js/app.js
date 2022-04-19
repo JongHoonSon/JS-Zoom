5ef583847744b3cec87513b73ca2c8270e3f6dc6
JongHoonSon <roka1684@gmail.com>
2022-04-19 17:12:16 +0900

    띄워쓰기 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,7 +5,7 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message : " + message.data + "from the Server");
+  console.log("New message : " + message.data + " from the Server");
 });
 
 socket.addEventListener("close", () => {
