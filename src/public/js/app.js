64fd2aebee5a520b39a3e4544d75424fa7fb9476
JongHoonSon <roka1684@gmail.com>
2022-04-19 16:51:12 +0900

    #1.4 WebSocket Messages
    브라우저에서 웹소켓이 보낸 메시지를 받음

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,3 +3,7 @@ const socket = new WebSocket(`ws://${window.location.host}`);
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 });
+
+socket.addEventListener("message", (message) => {
+  console.log("Just got this: " + message + "from the Server");
+});
