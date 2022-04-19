20ed3d5bd351e11a5e67b0e6dc0214bf60267443
JongHoonSon <roka1684@gmail.com>
2022-04-19 18:07:54 +0900

    if문 -> switch문으로 변경

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import http from "http";
+import { parse } from "path";
 import WebSocket from "ws";
 
 const app = express();
@@ -22,10 +23,11 @@ wss.on("connection", (socket) => {
   socket.on("close", () => console.log("Disconnected from the Browser"));
   socket.on("message", (message) => {
     const parsed = JSON.parse(message.toString());
-    if (parsed.type === "new_message") {
-      sockets.forEach((aSocket) => aSocket.send(parsed.payload));
-    } else if (parsed.type === "nickname") {
-      console.log(parsed.payload);
+    switch (parse.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+      case "nickname":
+        console.log(parsed.payload);
     }
   });
   socket.send("Hello! I'm WebSocket");
