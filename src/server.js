83c037ea1e8adf123657f52319c57169c3dc1dc5
JongHoonSon <roka1684@gmail.com>
2022-04-20 22:24:19 +0900

    #2.1 Installing SocketIO
    클라이언트에서 서버로 SocketIO를 메시지 전송 및
    서버는 받은 메시지를 출력
    
    SocketIO 메시지와 WebSocket 메시지의 차이점
    
    1. send가 아닌 emit으로 전송
    2. 사용자가 원하는 id로 보낼 수 있음 (WebSocket은 message로만 가능)
    3. 자바스크립트 객체를 전송할 수 있음
    4. JSON으로 형변환할 필요x (알아서 해줌)

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (msg) => console.log(msg));
 });
 
 // const wss = new WebSocket.Server({ server });
