f82cffe792f85f0e5652a34494d06f6bae60c5d6
JongHoonSon <roka1684@gmail.com>
2022-04-22 00:07:11 +0900

    #2.7 Nicknames
    소켓이 연결되거나 연결이 끊어질 때 해당 소켓의 닉네임을 client에게 전달

--- src/server.js
+++ src/server.js
@@ -23,11 +23,13 @@ wsServer.on("connection", (socket) => {
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
     done();
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
   });
   socket.on("new_message", (msg, room, done) => {
     socket.to(room).emit("new_message", `${socket.nickname}: msg`);
