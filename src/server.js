ef65d38c08c8f44d47ac141ec63d99fc54e541a5
JongHoonSon <roka1684@gmail.com>
2022-04-21 23:26:59 +0900

    #2.5 Room Messages
    socket이 room에 입장할 경우 해당 room에 welcome이라는 이벤트를 발생시킴

--- src/server.js
+++ src/server.js
@@ -22,6 +22,7 @@ wsServer.on("connection", (socket) => {
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
+    socket.to(roomName).emit("welcome");
     done();
   });
 });
