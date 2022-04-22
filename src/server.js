4f10c958f9cea29d29fc9d61b16ed5c56f22ee01
JongHoonSon <roka1684@gmail.com>
2022-04-22 21:24:49 +0900

    #3.0 User Video
    앱 초기화 (채팅 관련 기능 삭제)

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import http from "http";
 import { parse } from "path";
-import WebSocket from "ws";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import { SocketIO } from "socket.io";
 
 const app = express();
 
@@ -12,87 +10,8 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3001`);
-
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
-instrument(wsServer, {
-  auth: false,
-});
+const wsServer = SocketIO(httpServer);
 
-function publicRooms() {
-  const sids = wsServer.sockets.adapter.sids; // adapter 안에 있는 socket의 id 정보
-  const rooms = wsServer.sockets.adapter.rooms; // adapter 안에 있는 room 정보
-
-  const publicRooms = [];
-  rooms.forEach((value, key) => {
-    // 모든 룸 중에서
-    if (sids.get(key) === undefined) {
-      // 룸의 id가 sids에 있지 않은 것을 (private room 제외)
-      publicRooms.push(key); // public room에 추가
-    }
-  });
-  return publicRooms;
-}
-
-function countUser(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anonymous";
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    socket.to(roomName).emit("welcome", socket.nickname, countUser(roomName));
-    done();
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countUser(room) - 1)
-    );
-  });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-// const wss = new WebSocket.Server({ server });
-
-// const sockets = [];
-
-// wss.on("connection", (socket) => {
-//   sockets.push(socket);
-//   socket["nickname"] = "Anonymous";
-//   console.log("Connected to Browser");
-//   socket.on("close", () => console.log("Disconnected from the Browser"));
-//   socket.on("message", (msg) => {
-//     const message = JSON.parse(msg.toString());
-//     switch (message.type) {
-//       case "new_message":
-//         sockets.forEach((aSocket) =>
-//           aSocket.send(`${socket.nickname} : ${message.payload}`)
-//         );
-//         break;
-//       case "nickname":
-//         socket["nickname"] = message.payload;
-//         break;
-//     }
-//   });
-//   socket.send("Hello! I'm WebSocket");
-// });
-
+const handleListen = () => console.log(`Listening on http://localhost:3001`);
 httpServer.listen(3001, handleListen);
