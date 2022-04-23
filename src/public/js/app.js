77a35debce5c3647102214506eeb9a215be9a177
JongHoonSon <roka1684@gmail.com>
2022-04-23 09:58:10 +0900

    #3.7 IceCandidate
    myPeerConnection을 생성한 뒤에 바로 myPeerConnection이 icecandidate 이벤트를 listen하도록 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -138,7 +138,12 @@ socket.on("answer", (answer) => {
 
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log(data);
 }
